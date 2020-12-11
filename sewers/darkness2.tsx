6484af122a1aa1e8c6242a8d004f1c457d0dce71
Lukas Ruge <lukeslog@googlemail.com>
2020-12-12 00:09:13 +0100

    more stuff.

---
+++ sewers/darkness2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="darkness2" tilewidth="32" tileheight="32" tilecount="10" columns="1">
+ <image source="darkness2.png" width="32" height="320"/>
+</tileset>
