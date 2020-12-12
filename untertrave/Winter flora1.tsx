460d864018983efa09522c10dadcbbbc7b6b84cc
Lukas Ruge <lukeslog@googlemail.com>
2020-12-12 21:45:22 +0100

    ...

---
+++ untertrave/Winter flora1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Winter flora1" tilewidth="32" tileheight="32" tilecount="266" columns="14">
+ <image source="Winter flora1.png" width="468" height="636"/>
+</tileset>
