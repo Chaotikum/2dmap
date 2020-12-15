73c224c8457fad59afe0d2358df5c2f671a0126b
Lukas Ruge <lukeslog@googlemail.com>
2020-12-15 14:04:15 +0100

    sewers2

---
+++ sewers2/SkeletonC.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="SkeletonC" tilewidth="32" tileheight="32" tilecount="3" columns="1">
+ <image source="SkeletonC.png" width="54" height="108"/>
+</tileset>
