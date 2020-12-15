a3bf3839383e7fbb4d5825ee95a4571d33198965
k <k@debi3>
2020-12-15 20:27:07 +0100

    added nbsp mittelalt map

---
+++ nbsp_mittelalt/Slates v2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Slates v2" tilewidth="32" tileheight="32" tilecount="1288" columns="56">
+ <image source="../../documents-export-2020-12-15/Slates v2/Slates v.2 [32x32px orthogonal tileset by Ivan Voirol].png" width="1792" height="736"/>
+</tileset>
