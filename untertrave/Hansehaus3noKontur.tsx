789ff984efefe393a95e749b008612d4b170471c
Lukas Ruge <lukeslog@googlemail.com>
2020-12-16 22:45:53 +0100

    Removed test directory

--- untertrave/Hansehaus3noKontur.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="Hansehaus3noKontur" tilewidth="32" tileheight="32" tilecount="36" columns="6">
- <image source="Hansehaus3noKontur.png" width="192" height="192"/>
-</tileset>
