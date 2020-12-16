0d50f972bcd9628b8cde875917b17440efd003ff
Lukas Ruge <lukeslog@googlemail.com>
2020-12-16 22:48:27 +0100

    remove trave folder, has been renamed

--- trave/ADWinter_TileA5_32.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="ADWinter_TileA5_32" tilewidth="32" tileheight="32" tilecount="288" columns="12">
- <image source="ADWinter_TileA5_32.png" width="384" height="768"/>
-</tileset>
