8f6ec0ff96086a9628924710a7bb55a641d3737e
Lukas Ruge <lukeslog@googlemail.com>
2020-12-16 13:51:11 +0100

    renaming and stuff

---
+++ luebeck/fackeln.tsx
@@ -0,0 +1,46 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="fackeln" tilewidth="32" tileheight="32" tilecount="100" columns="10">
+ <image source="fackeln.png" width="320" height="320"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="200"/>
+   <frame tileid="1" duration="200"/>
+   <frame tileid="2" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="4">
+  <animation>
+   <frame tileid="5" duration="200"/>
+   <frame tileid="4" duration="200"/>
+   <frame tileid="6" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="10">
+  <animation>
+   <frame tileid="12" duration="200"/>
+   <frame tileid="10" duration="200"/>
+   <frame tileid="11" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="14">
+  <animation>
+   <frame tileid="15" duration="200"/>
+   <frame tileid="15" duration="200"/>
+   <frame tileid="16" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="21">
+  <animation>
+   <frame tileid="23" duration="200"/>
+   <frame tileid="22" duration="200"/>
+   <frame tileid="21" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="30">
+  <animation>
+   <frame tileid="30" duration="200"/>
+   <frame tileid="31" duration="200"/>
+   <frame tileid="32" duration="200"/>
+  </animation>
+ </tile>
+</tileset>
---
+++ trave/ADWinter_TileA3.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="ADWinter_TileA3" tilewidth="32" tileheight="32" tilecount="288" columns="24">
+ <image source="ADWinter_TileA3.png" width="768" height="384"/>
+</tileset>
